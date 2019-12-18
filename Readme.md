61eb461ddd5bb8bf1d97b688eb52b06853bb16f1
laurashub <shublaura@gmail.com>
2019-12-17 16:16:43 -0800

    Added writeup, cleaned up code, updated readme

--- Readme.md
+++ Readme.md
@@ -17,7 +17,11 @@
 * betweenness centrality
 * clustering coefficient
 
+### Text files
+{strain}_{metric}.txt - ranked reactions and essential classification. Bridging centrality and betweenness centrality sorted greatest to least, clustering coefficient sorted least to greatest
+
 ## Other files
 
-* overlap_pval.png
-* lshub_individual_analysis.txt
+* overlap_pval.png - overlap heatmap 
+* lshub_individual_analysis.pptx - individual presentation
+* lshub_individual_analysis.pdf - individual write up
--- enrichment_test.R
+++ enrichment_test.R
@@ -1,34 +1,13 @@
-install.packages("xtable")
-install.packages("digits")
-install.packages("tidyr")
-install.packages("tidyverse")
 library(ggplot2)
 library(xtable)
-library(tidyverse)
 
-library(tidyr)
 read_file <- function(filename){
  file_info <- read.table(filename, header = FALSE, sep = " ", dec = ".")
  return(file_info)
 }
 
-strains <- c('MG1655', 'W3110', 'EDL933', 'SAKAI', 'CFTO73', 'UTI89', 'core')
-metrics <- c('bridging_centrality', 'betweenness_centrality', 'clustering_coefficient')
-data = vector(mode = "list", length = (length(strains) * length(metrics)))
-i = 1
-for (strain in strains){
-  for (metric in metrics){
-    name <- sprintf('%s_%s.txt', strain, metric)
-    data[[i]] <- read_file(name)
-    i <- i+1
-  }
-}
 
-iJO1366_bridging_centrality <- read_file('iJO1366_bridging_centrality.txt')
-sum(iJO1366_bridging_centrality[,3] == 'False')
-#p-value you want is the probability of getting 100 or more white balls 
-#in a sample of size 400 from an urn with 3000 white balls and 12000 black balls
-
+#Enrichment for essential reactions in top 5% of ranked_data
 hypergeom_essential_enrichment <- function(ranked_data){
   essentials <- sum(ranked_data[,3] == 'True')
   nonessentials <- sum(ranked_data[,3] == 'False')
@@ -41,6 +20,22 @@ hypergeom_essential_enrichment <- function(ranked_data){
   return(pval)
 }
 
+#Enrichment for overlapping reactions in top 5% of data1 with data2
+hypergeom_overlap_enrichment <- function(data1, data2){ 
+  top1_0.05 <- data1[1:floor(dim(data1)[1]*0.05),1]
+  top2_0.05 <- data2[1:floor(dim(data2)[1]*0.05),1]
+  
+  essentials <- sum(sapply(data1, function(x){return(is.element(x, top2_0.05))}))
+  nonessentials <- dim(data1)[1] - essentials
+  
+  max_essentials <- length(top1_0.05)
+  top_essentials <- sum(sapply(top1_0.05, function(x){return(is.element(x, top2_0.05))}))
+  
+  pval <- sum(dhyper(top_essentials:max_essentials, essentials, nonessentials, max_essentials))
+  return(pval)
+}
+
+#fishers, used to check hypergeometric, returned same values
 #fishers_essential_enrichment <- function(ranked_data){
 #  top_0.05 <- ranked_data[1:floor(dim(ranked_data)[1]*0.05),]
 #  bottom_0.05 <- ranked_data[ceiling(dim(ranked_data)[1]*0.05):dim(ranked_data)[1],]
@@ -56,28 +51,29 @@ hypergeom_essential_enrichment <- function(ranked_data){
 #  print(ftest)
 #}
 
+#read all files into list of tables
+strains <- c('MG1655', 'W3110', 'EDL933', 'SAKAI', 'CFTO73', 'UTI89', 'core')
+metrics <- c('bridging_centrality', 'betweenness_centrality', 'clustering_coefficient')
+data = vector(mode = "list", length = (length(strains) * length(metrics)))
+i = 1
+for (strain in strains){
+  for (metric in metrics){
+    name <- sprintf('%s_%s.txt', strain, metric)
+    data[[i]] <- read_file(name)
+    i <- i+1
+  }
+}
+
+#calculate essential pvals for each list read in
 essential_pvals <- c(1:21)
 for (i in 1:21){
-  essential_pvals[i] <-calc_essential_enrichment(data[[i]])
+  essential_pvals[i] <-hypergeom_essential_enrichment(data[[i]])
 }
 
 essential_pvals <- matrix(unlist(essential_pvals), ncol = 3, byrow = TRUE)
 essential_pvals <- formatC(essential_pvals, format = "e", digits = 3)
-xtable(essential_pvals)
+xtable(essential_pvals) #format for latex
 
-hypergeom_overlap_enrichment <- function(data1, data2){ 
-  top1_0.05 <- data1[1:floor(dim(data1)[1]*0.05),1]
-  top2_0.05 <- data2[1:floor(dim(data2)[1]*0.05),1]
-  
-  essentials <- sum(sapply(data1, function(x){return(is.element(x, top2_0.05))}))
-  nonessentials <- dim(data1)[1] - essentials
-  
-  max_essentials <- length(top1_0.05)
-  top_essentials <- sum(sapply(top1_0.05, function(x){return(is.element(x, top2_0.05))}))
-  
-  pval <- sum(dhyper(top_essentials:max_essentials, essentials, nonessentials, max_essentials))
-  return(pval)
-}
 
 overlap_pvals  <- c(1:49) 
 index = 1
@@ -92,10 +88,9 @@ for (i in 0:6){
   }
 }
 
-overlap_pvals_matrix <- matrix(unlist(overlap_pvals), ncol = 7, byrow = TRUE, dimnames=list(strains, strains))
-
-overlap_pvals <- unlist(lapply(overlap_pvals, function(x){ -log10(x)}))
+#make heatmap from overlap_pvals
+overlap_pvals.log10 <- unlist(lapply(overlap_pvals, function(x){ -log10(x)}))
 overlap_pvals.df <- expand.grid(strain1 = strains, strain2 = strains)
-overlap_pvals.df$pval <- overlap_pvals
+overlap_pvals.df$pval <- overlap_pvals.log10
 ggplot(data = overlap_pvals.df, aes(x = strain1, y = strain2)) + geom_tile(aes(fill = pval)) 
 ggsave("overlap_pval.png")
